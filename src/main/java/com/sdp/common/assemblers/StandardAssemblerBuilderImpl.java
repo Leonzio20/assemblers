00c08a70b973a51af46298358730eba13a4f144b
Leonzio20 <adamograz@gmail.com>
2018-11-14 20:12:15 +0100

    Replace LogManager by lombok annotation

--- src/main/java/com/sdp/common/assemblers/StandardAssemblerBuilderImpl.java
+++ src/main/java/com/sdp/common/assemblers/StandardAssemblerBuilderImpl.java
@@ -7,17 +7,15 @@ import com.sdp.common.util.Getter;
 import com.sdp.common.util.ObjectConstructor;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author leonzio
  */
 @AllArgsConstructor
+@Log4j2
 final class StandardAssemblerBuilderImpl<S, T> implements StandardAssemblerBuilder<S, T>
 {
-  private static final Logger LOGGER = LogManager.getLogger(StandardAssemblerBuilder.class);
-
   private final Class<T> targetClass;
   private final AssembledFieldValidator<S> assembledFieldValidator;
   private final Set<ValueMapper<S, T, ?>> mappers;
@@ -25,7 +23,7 @@ final class StandardAssemblerBuilderImpl<S, T> implements StandardAssemblerBuild
   StandardAssemblerBuilderImpl(@NotNull Class<S> sourceClass, @NotNull Class<T> targetClass)
   {
     this(targetClass, new AssembledFieldValidator<>(sourceClass), ImmutableSet.of());
-    LOGGER.debug("Creating assembler from {} to {}.", sourceClass.getCanonicalName(), targetClass.getCanonicalName());
+    log.debug("Creating assembler from {} to {}.", sourceClass.getCanonicalName(), targetClass.getCanonicalName());
   }
 
   StandardAssemblerBuilderImpl(@NotNull StandardAssemblerBuilderImpl<S, T> source,
